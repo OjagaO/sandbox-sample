b2f2e31d2ae7f5d057ea4853e52a150c39873c07
OjagaO <113488745+OjagaO@users.noreply.github.com>
2023-02-03 11:11:26 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  私たちは更なる利便性を追い求めております参考にこちらをどうぞ
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
