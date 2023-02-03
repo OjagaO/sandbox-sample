eca9b953463f8e65749bbdc0234eb8a667980002
OjagaO <113488745+OjagaO@users.noreply.github.com>
2023-02-03 11:41:30 +0900

    JS,HT変更

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import "./styles.css";
 document.getElementById("app").innerHTML = `
 <h1>Hello Vanilla!</h1>
 <div>
-  私たちは更なる利便性を追い求めております参考にこちらをどうぞ
+  JSのコードを変更しました。
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
 `;
