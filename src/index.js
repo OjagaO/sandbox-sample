b730a83fa57378ede3282da2649c75ae52b83547
OjagaO <113488745+OjagaO@users.noreply.github.com>
2023-02-13 11:30:44 +0900

    add

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  JSのコードを変更しました。
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+let addBtn = document.getElementById("addBtn");
+
+let todoAdd = () => {
+  let input = document.getElementById("input");
+  let todoArea = document.getElementById("todo_area");
+  let val = input.value;
+  let li = document.createElement("li");
+  let compBtn = document.createElement("button");
+  compBtn.innerText = "完了";
+  compBtn.setAttribute("id", "complete");
+  let deleBtn = document.createElement("button");
+  deleBtn.innerText = "削除";
+  deleBtn.setAttribute("id", "delete");
+  li.append(compBtn);
+  li.innerText = val;
+  todoArea.append(li);
+  li.append(compBtn);
+  li.append(deleBtn);
+  input.value = "";
+};
+
+addBtn.addEventListener("click", function () {
+  todoAdd();
+});
+
+let complete = () => {};
